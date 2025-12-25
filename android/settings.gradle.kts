535eb0010c25401347a71241c147bb7407e2be72
innocentsamuel758 <innocentsamuel758@gmail.com>
2025-12-25 12:29:44 +0000

    AGP CHANGES

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // CORRECTED: Downgraded to a stable version compatible with Flutter.
+    id("com.android.application") version "8.2.2" apply false
+    // CORRECTED: Downgraded to a stable version compatible with AGP 8.2.2.
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 include(":app")
